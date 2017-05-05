8f2b09b1e5631145d3f12c328eb96123935acb69
山岡千晃 <chiakiyamaoka@MacBook-Pro.local>
2017-05-05 13:54:07 +0900

    カウントダウン機能の追加

--- AndroidStudioProjects/JankenGame/app/src/main/java/com/example/chiakiyamaoka/jankengame/JankenActivity.java
+++ AndroidStudioProjects/JankenGame/app/src/main/java/com/example/chiakiyamaoka/jankengame/JankenActivity.java
@@ -1,15 +1,16 @@
 package com.example.chiakiyamaoka.jankengame;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Random;
 
-public class JankenActivity extends AppCompatActivity {
+public class JankenActivity extends AppCompatActivity implements Runnable {
 
     Button mSelectRockButton;
     Button mSelectScissorsButton;
@@ -17,6 +18,7 @@ public class JankenActivity extends AppCompatActivity {
     Button mCancelButton;
     TextView mEnemyHand;
     TextView mCountText;
+    TextView mTimeText;
 
     String mJankenSet[] = {
             "ぐー",
@@ -26,11 +28,20 @@ public class JankenActivity extends AppCompatActivity {
 
     int mEnemyHandNum;
     int mCount = 0;
+    long mStartTime;
+    long mLimitTime = 6L;
+
+    Thread thread = null;
+    Handler handler = new Handler();
+    SimpleDateFormat mDataFormat = new SimpleDateFormat("ss.SS");
+    private long mPeriod = 10;
+    private boolean mStopRun = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_janken);
+
 
         mSelectRockButton = (Button) findViewById(R.id.rock);
         mSelectScissorsButton = (Button) findViewById(R.id.scissors);
@@ -38,7 +49,9 @@ public class JankenActivity extends AppCompatActivity {
         mCancelButton = (Button) findViewById(R.id.cancel_button);
         mEnemyHand = (TextView) findViewById(R.id.enemy_hand);
         mCountText = (TextView) findViewById(R.id.count);
+        mTimeText = (TextView) findViewById(R.id.time);
 
+        startTime();
         setEnemyHand();
 
         mSelectRockButton.setOnClickListener(onClickHand);
@@ -52,6 +65,12 @@ public class JankenActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void startTime() {
+        thread = new Thread(this);
+        thread.start();
+        mStartTime = System.currentTimeMillis();
     }
 
     private void setEnemyHand() {
@@ -88,4 +107,28 @@ public class JankenActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void run() {
+
+        while (!mStopRun) {
+            try {
+                Thread.sleep(mPeriod);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                mStopRun = true;
+            }
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    long endTime = System.currentTimeMillis();
+                    long elapseTime = (mLimitTime - endTime - mStartTime);
+                    /*if (elapseTime < 0L) {
+                        mStopRun = true;
+                    }*/
+                    mTimeText.setText("Time:" + mDataFormat.format(elapseTime));
+                }
+            });
+        }
+    }
 }
